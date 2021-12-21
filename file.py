db9cb86cdea61d1963c3a6deb64b7c35e15cc190
mugen1337 <53204695+mugen1337@users.noreply.github.com>
2021-12-21 14:34:22 +0900

    Update file.py

--- file.py
+++ file.py
@@ -4,3 +4,10 @@ import glob
 # https://note.nkmk.me/python-pathlib-iterdir-glob/
 file_li = glob.glob("./*.txt")
 
+
+# int, 改行区切りのファイル，リストに読む
+def read(path):
+    with open(path) as f:
+        li = [int(s.strip()) for s in f.readlines()]
+
+    print(li)
